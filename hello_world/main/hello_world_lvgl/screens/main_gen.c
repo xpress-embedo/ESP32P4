4d6a44d7fc2eef8fdaeee498e5d28c94db9abb11
abc <abc@xyz.com>
2025-09-08 17:53:04 +0530

    Hello World: Build Error Fixed, but still not working

--- hello_world/main/hello_world_lvgl/screens/main_gen.c
+++ hello_world/main/hello_world_lvgl/screens/main_gen.c
@@ -35,13 +35,10 @@ lv_obj_t * main_create(void)
 {
     LV_TRACE_OBJ_CREATE("begin");
 
-    static lv_style_t main;
 
     static bool style_inited = false;
 
     if (!style_inited) {
-        lv_style_init(&main);
-        lv_style_set_text_font(&main, font_title);
 
         style_inited = true;
     }
@@ -49,7 +46,6 @@ lv_obj_t * main_create(void)
     lv_obj_t * lv_obj_0 = lv_obj_create(NULL);
     lv_obj_set_width(lv_obj_0, lv_pct(100));
     lv_obj_set_height(lv_obj_0, lv_pct(100));
-    lv_obj_add_style(lv_obj_0, &main, 0);
 
     lv_obj_t * lv_label_0 = lv_label_create(lv_obj_0);
     lv_label_set_text(lv_label_0, "Hello World Using LVGL Editor");
