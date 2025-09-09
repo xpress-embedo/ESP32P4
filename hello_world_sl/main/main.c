204ecc05a9ec2e7b9819f754441fa0c268132c7e
abc <abc@xyz.com>
2025-09-09 20:40:32 +0530

    Hello World Square Line: Reading is working but not completely, will investigate later

--- hello_world_sl/main/main.c
+++ hello_world_sl/main/main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 
 #include "esp_log.h"
+#include "esp_partition.h"
+
 #include "freertos/idf_additions.h"
 
 #include "gui_mng.h"
@@ -13,10 +15,29 @@
 // Private Variables
 static const char *TAG = "MAIN";
 
+
+void read_config()
+{
+  const esp_partition_t* config_partition = esp_partition_find_first( ESP_PARTITION_TYPE_DATA, 0x40, "config" );
+
+  if ( config_partition )
+  {
+    uint8_t buffer[80]; // Adjust size as needed
+    esp_partition_read(config_partition, 0, buffer, sizeof(buffer));
+    ESP_LOGI( TAG, "Read config data: %s", buffer );
+  }
+  else
+  {
+    ESP_LOGE( TAG, "Config partition not found!" );
+  }
+}
+
 // Private Function Declarations
 void app_main(void)
 {
   ESP_LOGI( TAG, "Starting Program" );
+
+  read_config();
   
   // start the gui task
   gui_start();
