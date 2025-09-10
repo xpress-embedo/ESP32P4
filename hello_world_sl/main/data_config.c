c83be050be73be5f31eae95a94bab4cd6eea684f
abc <abc@xyz.com>
2025-09-10 10:34:01 +0530

    Hello World Square Line: function header update and function definition added

--- hello_world_sl/main/data_config.c
+++ hello_world_sl/main/data_config.c
@@ -11,6 +11,9 @@
 #include "esp_partition.h"
 
 // Private Macros
+// Uncomment this line to print all the data read from configuration
+#define DATA_CONFIG_DEBUG_EN
+
 #define MAX_ACCESS_POINT_NAME_LEN         (32)
 #define MAX_IP_ADDRESS_LEN                (16)
 #define MAX_SERIAL_NUMBER_LEN             (32)
@@ -19,16 +22,22 @@
 // Padding shouldn't be used for RAW data storage
 typedef struct __attribute__((packed)) 
 {
-  char access_point_name[MAX_ACCESS_POINT_NAME_LEN];
-  char ip_address[MAX_IP_ADDRESS_LEN];
+  char ap_name[MAX_ACCESS_POINT_NAME_LEN];
+  char ap_ip[MAX_IP_ADDRESS_LEN];
   char serial_number[MAX_SERIAL_NUMBER_LEN];
 } device_config_t;
 
 // Private Variables
 static const char *TAG = "CFG";
+static bool is_initialized = false;
 static device_config_t device_cfg;
 
 // Public Function Definitions
+
+/**
+ * @brief This function initializes the configuration data by reading from the "config" partition.
+ * @param  None
+ */
 void cfg_init( void )
 {
   const esp_partition_t* config_partition = esp_partition_find_first( ESP_PARTITION_TYPE_DATA, 0x40, "config" );
@@ -45,20 +54,49 @@ void cfg_init( void )
     ESP_LOGE( TAG, "Failed to read config data: %s", esp_err_to_name(err) );
     return;
   }
-  ESP_LOGI(TAG, "Access Point Name: %s", device_cfg.access_point_name);
-  ESP_LOGI(TAG, "IP Address: %s", device_cfg.ip_address);
-  ESP_LOGI(TAG, "Serial Number: %s", device_cfg.serial_number);
+  is_initialized = true;
+  #ifdef DATA_CONFIG_DEBUG_EN
+  ESP_LOGW(TAG, "Access Point Name: %s", device_cfg.ap_name);
+  ESP_LOGW(TAG, "IP Address: %s", device_cfg.ap_ip);
+  ESP_LOGW(TAG, "Serial Number: %s", device_cfg.serial_number);
+  #endif
 }
 
+/**
+ * @brief This function returns the device default access point name
+ * @param access_point_name 
+ */
 void cgf_get_access_point( char *access_point_name )
 {
+  if ( is_initialized )
+  {
+    strncpy(access_point_name, device_cfg.ap_name, MAX_ACCESS_POINT_NAME_LEN);
+  }
 }
 
+/**
+ * @brief This function returns the device default access point IP address
+ * @param ip_address 
+ */
 void cgf_get_ip_address( char *ip_address )
 {
+  if ( is_initialized )
+  {
+    strncpy(ip_address, device_cfg.ap_ip, MAX_IP_ADDRESS_LEN);
+  }
 }
 
+/**
+ * @brief This function returns the device serial number
+ * @param serial_number 
+ */
 void cgf_get_serial_number( char *serial_number )
 {
-
+  if ( is_initialized )
+  {
+    strncpy(serial_number, device_cfg.serial_number, MAX_SERIAL_NUMBER_LEN);
+  }
 }
+
+// Private Function Definitions
+// todo
